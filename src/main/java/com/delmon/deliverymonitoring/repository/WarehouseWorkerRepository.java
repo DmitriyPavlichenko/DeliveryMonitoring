ea10d45d0bac628d90d8b83470db0d1f63cfb5b5
Dmitriy <pavlichenko.dmitriy@gmail.com>
2021-11-15 14:40:34 +0200

    Added repositories

--- src/main/java/com/delmon/deliverymonitoring/repository/WarehouseWorkerRepository.java
+++ src/main/java/com/delmon/deliverymonitoring/repository/WarehouseWorkerRepository.java
@@ -1,7 +1,8 @@
 package com.delmon.deliverymonitoring.repository;
 
+import com.delmon.deliverymonitoring.model.WarehouseWorker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WarehouseWorkerRepository extends JpaRepository<WarehouseWorkerRepository, Long> { }
+public interface WarehouseWorkerRepository extends JpaRepository<WarehouseWorker, Long> { }
