ce0dd45ac21e50980aa34056d6a2ac2866747792
Dmitriy <pavlichenko.dmitriy@gmail.com>
2021-11-15 18:50:08 +0200

    Some minor changes

--- src/test/java/com/delmon/deliverymonitoring/repository/OrderingRepositoryTest.java
+++ src/test/java/com/delmon/deliverymonitoring/repository/OrderingRepositoryTest.java
@@ -7,18 +7,13 @@ import com.delmon.deliverymonitoring.model.WarehouseWorker;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class OrderingRepositoryTest {
