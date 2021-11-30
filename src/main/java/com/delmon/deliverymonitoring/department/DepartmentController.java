e3aa1b25452ed8eef6215c701ab3c97af93a3aee
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2021-11-30 23:28:35 +0200

    Added DepartmentController without any functionality

---
+++ src/main/java/com/delmon/deliverymonitoring/department/DepartmentController.java
@@ -0,0 +1,8 @@
+package com.delmon.deliverymonitoring.department;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DepartmentController {
+    // TODO with only Role.PRODUCT_SUPPLIER access
+}
