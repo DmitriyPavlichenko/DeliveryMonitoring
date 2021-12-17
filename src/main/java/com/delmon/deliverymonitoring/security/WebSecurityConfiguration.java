05a6bd4f6fc2786bd1a9dd9d4a9839f98dff60a9
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2021-12-17 20:29:18 +0200

    Cord allowing

--- src/main/java/com/delmon/deliverymonitoring/security/WebSecurityConfiguration.java
+++ src/main/java/com/delmon/deliverymonitoring/security/WebSecurityConfiguration.java
@@ -28,6 +28,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
                     .disable()
                 .authorizeRequests()
