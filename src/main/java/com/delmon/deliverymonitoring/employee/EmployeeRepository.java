8b974a9f535d27b7abd1efc26ce8aa4694942b7f
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2021-12-23 14:32:01 +0200

    Delete method fixing

--- src/main/java/com/delmon/deliverymonitoring/employee/EmployeeRepository.java
+++ src/main/java/com/delmon/deliverymonitoring/employee/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.delmon.deliverymonitoring.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -9,6 +11,7 @@ import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
     Optional<Employee> findEmployeeByPhoneNumber(String phoneNumber);
     boolean existsByPhoneNumber(String phoneNumber);
+    @Transactional
     void deleteByPhoneNumber(String phoneNumber);
 
     Employee getByUuid(String employeeUuid);
