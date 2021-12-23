ae54d7de14c1bdf999f46828a08b46642df3d620
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2021-12-23 23:32:53 +0200

    Fixed authentication response for default admin user

--- src/main/java/com/delmon/deliverymonitoring/authentication/AuthenticationService.java
+++ src/main/java/com/delmon/deliverymonitoring/authentication/AuthenticationService.java
@@ -3,8 +3,11 @@ package com.delmon.deliverymonitoring.authentication;
 import com.delmon.deliverymonitoring.security.user.ApplicationUser;
 import com.delmon.deliverymonitoring.security.user.ApplicationUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import static com.delmon.deliverymonitoring.employee.EmployeeRole.ADMINISTRATOR;
 
 @AllArgsConstructor
 @Service
@@ -12,8 +15,19 @@ public class AuthenticationService {
     private ApplicationUserService userService;
 
     public AuthenticationResponse authenticate() {
-        ApplicationUser user =
-                userService.findUserByPhoneNumber(SecurityContextHolder.getContext().getAuthentication().getName());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if ("admin".equals(authentication.getName())) {
+            return new AuthenticationResponse(
+                    "admin",
+                    "adminFName",
+                    "adminLName",
+                    ADMINISTRATOR,
+                    false,
+                    true
+            );
+        }
+
+        ApplicationUser user = userService.findUserByPhoneNumber(authentication.getName());
         return new AuthenticationResponse(
                 user.getEmployee().getPhoneNumber(),
                 user.getEmployee().getFirstName(),
