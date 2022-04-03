9297d287aaef4a0b37cc21e7a35df4166f8ed19f
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2022-04-03 15:45:24 +0300

    Deleted unused method

--- src/main/java/com/delmon/deliverymonitoring/security/user/ApplicationUserRepository.java
+++ src/main/java/com/delmon/deliverymonitoring/security/user/ApplicationUserRepository.java
@@ -1,8 +1,6 @@
 package com.delmon.deliverymonitoring.security.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,14 +9,8 @@ import java.util.Optional;
 @Repository
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, String> {
     Optional<ApplicationUser> findAppUserByEmployee_PhoneNumber(String phoneNumber);
-    boolean existsByEmployee_PhoneNumber(String phoneNumber);
 
-    @Modifying(clearAutomatically = true)
-    @Transactional
-    @Query("UPDATE ApplicationUser u " +
-            "SET u.enabled = ?2 " +
-            "WHERE u.employee.phoneNumber = ?1")
-    int updateEnabledByPhoneNumber(String email, boolean enabled);
+    boolean existsByEmployee_PhoneNumber(String phoneNumber);
 
     @Transactional
     void deleteByEmployee_PhoneNumber(String phoneNumber);
