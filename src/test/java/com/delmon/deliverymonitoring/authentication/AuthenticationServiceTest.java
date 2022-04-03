e9f4109cdffdc4bf9823cbe31446fa9765b6b46d
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2022-04-03 15:50:06 +0300

    Authentication tests

--- src/test/java/com/delmon/deliverymonitoring/authentication/AuthenticationServiceTest.java
+++ src/test/java/com/delmon/deliverymonitoring/authentication/AuthenticationServiceTest.java
@@ -1,22 +1,23 @@
 package com.delmon.deliverymonitoring.authentication;
 
+import com.delmon.deliverymonitoring.employee.Employee;
+import com.delmon.deliverymonitoring.security.user.ApplicationUser;
 import com.delmon.deliverymonitoring.security.user.ApplicationUserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import static com.delmon.deliverymonitoring.employee.EmployeeRole.ADMINISTRATOR;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationServiceTest {
@@ -26,18 +27,18 @@ class AuthenticationServiceTest {
 
     @BeforeEach
     void setUp() {
-        service = new AuthenticationService(applicationUserService);
-    }
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
 
-    @Mock
-    private Authentication authentication;
+        service = new AuthenticationService(applicationUserService);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+    }
 
     @Test
     void canAuthenticateAsAdmin() {
-        // given
-        given(authentication.getName()).willReturn("admin");
-
         // when
+        when(SecurityContextHolder.getContext().getAuthentication().getName()).thenReturn("admin");
         AuthenticationResponse response = service.authenticate();
 
         // then
@@ -45,6 +46,32 @@ class AuthenticationServiceTest {
                 "admin",
                 "adminFName",
                 "adminLName",
+                ADMINISTRATOR,
+                false,
+                true
+        ));
+    }
+
+    @Test
+    void canAuthenticateAsUser() {
+        // given
+        given(applicationUserService.findUserByPhoneNumber(anyString())).willReturn(new ApplicationUser(new Employee(
+                "firstName",
+                "lastName",
+                ADMINISTRATOR,
+                "number"),
+                "pass"
+        ));
+
+        // when
+        when(SecurityContextHolder.getContext().getAuthentication().getName()).thenReturn("anyString()");
+        AuthenticationResponse response = service.authenticate();
+
+        // then
+        assertThat(response).isEqualTo(new AuthenticationResponse(
+                "number",
+                "firstName",
+                "lastName",
                 ADMINISTRATOR,
                 false,
                 true
